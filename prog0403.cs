56478ec38622d5f1d47814f68e5d8019417f5aaa
Vinicius de Camargo Fantin <415238@unoesc.lan>
2024-03-04 20:40:27 -0300

    tipos numericos

--- prog0403.cs
+++ prog0403.cs
@@ -9,3 +9,18 @@ bool isNomeEqual2 = string.Equals(nome completo, "Vinicius Ratão Fantin");
 
 console.WriteLine($" Primeiro resultado: {isNomeEquals}");
 console.WriteLine($" Segundo resultado: {isNomeEquals2}");
+
+/* 
+TIPOS NUMERICOS
+
+    style: Armazena valores entre -128 e 127
+    short: Armazena valores entre -32,768 e 32,767
+    int: Armazena valores entre -2,147,483,684 e 2,147,483,684
+    long: Armazena valores entre -9,223,372,036,854,775,808 e 9,223,372,036,854,775,808
+
+*/
+
+float myFloat = 10.0f;
+double myDouble = 5d;
+decimal myDecimal = 7m;
+int myInteger = 0;
