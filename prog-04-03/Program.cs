56922cc61a6202feded9cc8915422016a34ae098
Vinicius de Camargo Fantin <415238@unoesc.lan>
2024-03-04 20:21:43 -0300

    segundo do dia

---
+++ prog-04-03/Program.cs
@@ -0,0 +1,11 @@
+String nomeCompleto = "Vinicius Fantin";
+nomeCompleto = nomeCompleto.Replace("Bicho" , "Ratão");
+COnsole.WriteLine(nomeCompleto);
+
+//Comparação de String
+bool isNomeEqual = (nomecompleto == "Vinicius Fantin");
+
+bool isNomeEqual2 = string.Equals(nome completo, "Vinicius Ratão Fantin");
+
+console.WriteLine($" Primeiro resultado: {isNomeEquals}");
+console.WriteLine($" Segundo resultado: {isNomeEquals2}");
